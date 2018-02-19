af6f04fc77ed23357853466a97318b5994be2300
olialls <smallcowden@gmail.com>
2018-02-19 13:29:01 +0000

    Login

--- app/src/main/java/com/example/group5/focus/Stock_Page.java
+++ app/src/main/java/com/example/group5/focus/Stock_Page.java
@@ -1,29 +1,60 @@
 package com.example.group5.focus;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Stock_Page extends AppCompatActivity {
+    Button button;
+    TextView stockname, stockprice;
+    String json_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=15min&outputsize=compact&apikey=AUEWQ800Q3XMU1P0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock__page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        button = (Button)findViewById(R.id.RequestButton);
+        stockname = (TextView)findViewById(R.id.StockName);
+        stockprice = (TextView)findViewById(R.id.StockPrice);
+        button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, json_url, null)
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+                            stockname.setText(response.getString("Symbol"));
+                            stockprice.setText(response.getString("close"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                };
+                )
+
             }
         });
-    }
 
+
+    }
 }
+
+
